5e852f6ee84611009ca02d273fd90d89f97f2d9c
danielrfjr <157657284+danielrfjr@users.noreply.github.com>
2024-07-11 20:20:27 -0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,19 @@
+package com.forumhub.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Constructors, getters, and setters
+}
