2268aa291ad9b9e95711ffb1f6d4ee43f10d585c
danielrfjr <157657284+danielrfjr@users.noreply.github.com>
2024-07-11 20:19:45 -0300

    Create TopicResponse.java

---
+++ TopicResponse.java
@@ -0,0 +1,13 @@
+package com.forumhub.dto;
+
+public class TopicResponse {
+
+    private String titulo;
+    private String mensagem;
+    private String dataCriacao;
+    private String status;
+    private String autor;
+    private String curso;
+
+    // Getters and setters
+}
